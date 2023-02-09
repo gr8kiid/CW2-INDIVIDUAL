e09791653208eb90c3043be180587dc4ca7704f2
gr8kiid <yungcee@hotmail.com>
2023-02-09 15:43:09 +0400

    fix images middleware

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use('/images', express.static(path.join(__dirname, '../images')))
+app.use('/images', express.static(path.join(__dirname, '/images')))
 
 app.use((req, res, next) =>{
     res.status(404).send('Image not found.')
